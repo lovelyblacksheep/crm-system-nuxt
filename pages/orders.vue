133851e742d8b3587d5846197c8d9853ddeccef2
lovelyblacksheep <blacksheepmember@gmail.com>
2024-12-24 00:26:44 +0100

    first commit

---
+++ pages/orders.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-10">
+    <h1 class="font-bold text-2xl mb-10">Заказы</h1>
+  </div>
+</template>
---
+++ pages/products.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-10">
+    <h1 class="font-bold text-2xl mb-10">Товары</h1>
+  </div>
+</template>
